86fe97c888b6ca072a9844214ca9a8269ab8e3d8
ricardo-rocker <ricardo.santos.rocker@gmail.com>
2022-08-06 19:00:18 -0300

    Introdução a Components

---
+++ primeiro-projeto-angular/src/app/title/title.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+  title: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.title = "Bem vindo";
+  }
+
+}
